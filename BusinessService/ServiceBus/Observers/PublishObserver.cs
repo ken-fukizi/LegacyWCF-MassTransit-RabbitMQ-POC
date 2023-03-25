caa594c704660ad42ab8a55d7d6fd0cb21457a54
Kenneth Fukizi <ken.fukizi@afrikancoder.com>
2023-03-26 01:29:40 +0200

    Publish observer

---
+++ BusinessService/ServiceBus/Observers/PublishObserver.cs
@@ -0,0 +1,37 @@
+﻿using MassTransit;
+using MassTransit.Logging;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace BusinessService.ServiceBus.Observers
+{
+    public class PublishObserver : IPublishObserver
+    {
+        private readonly ILogger<PublishObserver> _logger;
+        public PublishObserver(ILogger<PublishObserver> logger)
+        {
+            _logger = logger;
+        }
+        public Task PostPublish<T>(PublishContext<T> context) where T : class
+        {
+            _logger.LogTrace($"POST PUBLISH: {context.ConversationId} /// {context.MessageId} ");
+            return Task.CompletedTask;
+        }
+
+        public Task PrePublish<T>(PublishContext<T> context) where T : class
+        {
+            _logger.LogTrace($"PRE PUBLISH: {context.ConversationId} /// {context.MessageId} ");
+            return Task.CompletedTask;
+        }
+
+        public Task PublishFault<T>(PublishContext<T> context, Exception exception) where T : class
+        {
+            _logger.LogTrace($"PUBLISH FAULT: {context.ConversationId} /// {context.MessageId} /// {exception.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
