adbfbb54143e91d15fef55c614540818badd7fb5
Kenneth Fukizi <ken.fukizi@afrikancoder.com>
2023-03-27 20:15:32 +0200

    Persisting message into queue, by binding the message command to the consumer

--- BusinessService/App_Start/NinjectWebCommon.cs
+++ BusinessService/App_Start/NinjectWebCommon.cs
@@ -71,6 +71,48 @@ namespace BusinessService.App_Start
             kernel.Bind<ILogger>().ToProvider(typeof(Logger<>));
             kernel.Bind<ITester>().To<Tester>();
             
+
+            kernel.Bind<IConsumer<ISaveCustomerLeadCommand>>().To<SaveCustomerLeadCommandConsumer>();
+
+            //var busControl =  Bus.Factory.CreateUsingRabbitMq(cfg =>
+            //{
+            //    var host = cfg.Host(new Uri("rabbitmq://localhost"), h =>
+            //    {
+            //        h.Username("guest");
+            //        h.Password("guest");
+            //    });
+            //    cfg.AutoDelete = false;
+            //    cfg.Durable = true;
+
+
+            //    cfg.ReceiveEndpoint(host, "save_customer_lead", e =>
+            //    {
+            //        e.AutoDelete = false;
+            //        e.PrefetchCount = 5;
+            //        e.UseRetry(retry =>
+            //        {
+            //            retry.Interval(3, TimeSpan.FromMinutes(5));
+            //            retry.Handle<Exception>();
+            //        });
+            //        e.UseCircuitBreaker(breaker =>
+            //        {
+            //            breaker.Handle<Exception>();
+            //            breaker.TrackingPeriod = TimeSpan.FromMinutes(1);
+            //            breaker.TripThreshold = 10;
+            //            breaker.ActiveThreshold = 10;
+            //            breaker.ResetInterval = TimeSpan.FromMinutes(1);
+            //        });
+            //        e.UseRateLimit(
+            //                rateLimit: 10,
+            //                interval: TimeSpan.FromSeconds(1)
+            //            );
+            //        e.Consumer<SaveCustomerLeadCommandConsumer>(kernel);
+            //        EndpointConvention.Map<ISaveCustomerLeadCommand>(e.InputAddress);
+            //    });
+            //});
+
+            //busControl.Start();
+
             // The concrete values will have to be moved to appsettings
             kernel.Bind<IBus>().ToMethod(ctx => Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
@@ -81,7 +123,8 @@ namespace BusinessService.App_Start
                 });
                 cfg.AutoDelete = false;
                 cfg.Durable = true;
-                
+
+
                 cfg.ReceiveEndpoint(host, "save_customer_lead", e =>
                 {
                     e.AutoDelete = false;
@@ -89,7 +132,7 @@ namespace BusinessService.App_Start
                     e.UseRetry(retry =>
                     {
                         retry.Interval(3, TimeSpan.FromMinutes(5));
-                        retry.Handle<Exception>();                       
+                        retry.Handle<Exception>();
                     });
                     e.UseCircuitBreaker(breaker =>
                     {
@@ -102,7 +145,7 @@ namespace BusinessService.App_Start
                     e.UseRateLimit(
                             rateLimit: 10,
                             interval: TimeSpan.FromSeconds(1)
-                        );                   
+                        );
                     e.Consumer<SaveCustomerLeadCommandConsumer>(kernel);
                     EndpointConvention.Map<ISaveCustomerLeadCommand>(e.InputAddress);
                 });
@@ -111,9 +154,7 @@ namespace BusinessService.App_Start
             kernel.Bind<ISendObserver>().To<SendObserver>();
             kernel.Bind<IReceiveObserver>().To<ReceiveObserver>();
             kernel.Bind<IPublishObserver>().To<PublishObserver>();
-            kernel.Bind<IBusObserver>().To<BusObserver>();
-
-            //addMassTransitHostedService
+            kernel.Bind<IBusObserver>().To<BusObserver>();            
         }        
     }
 }
